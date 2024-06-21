b9578e5cf5884d095f331264d2716fc57ab154b4
Alex Idowu <alexidowu25@gmail.com>
2024-06-21 16:26:34 +0100

    Update entrypoint.sh

--- buildkit/entrypoint.sh
+++ buildkit/entrypoint.sh
@@ -7,7 +7,9 @@ rm -rf /var/lib/buildkit/runc-native || true
 # Format the buildkit storage mount for better inode ratio
 if ! [ -f /var/lib/buildkit/.formatted ]; then
   umount /var/lib/buildkit || true
-  mkfs.ext4 -T news -F /dev/vdb
-  mount /dev/vdb /var/lib/buildkit
+  mkfs.ext4 -i 2048 -F /dev/vdb || true
+  mount /dev/vdb /var/lib/buildkit || true
   touch /var/lib/buildkit/.formatted
 fi
+
+exec buildkitd "$@"
